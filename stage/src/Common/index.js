4a2703187c2d8ac04c46ce399fffbd70053c46db
dou <978070462@qq.com>
2020-06-10 13:31:25 +0800

    保存测试接口地址

--- stage/src/Common/index.js
+++ stage/src/Common/index.js
@@ -8,8 +8,8 @@ export default class Common {
 	static cameraUrl = Common.protocol + window.location.host + "/stage/MyCamera.swf";
 	static customPrintUrl = Common.protocol + window.location.host + "/card/custom/";
 
-	static url = "http://www.coolvisit.top/qcvisitBase/";
-	// static url = "http://www.coolvisit.top/wmd/qcvisit/";
+	// static url = "http://www.coolvisit.top/qcvisitBase/";
+	static url = "http://www.coolvisit.top/wmd/qcvisit/";
 	// static url = "http://www.coolvisit.top/ykt/qcvisit/";
 	// static url = "http://139.217.223.183/qcvisit/";
 	// static printUrl = "http://www.coolvisit.top/card/index.html";
