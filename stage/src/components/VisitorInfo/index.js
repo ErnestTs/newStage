86725d843823a78ba415eb9706f6763fcdd2b570
dou <978070462@qq.com>
2020-07-03 11:38:20 +0800

    保存代码

--- stage/src/components/VisitorInfo/index.js
+++ stage/src/components/VisitorInfo/index.js
@@ -918,7 +918,7 @@ export default class VisitorInfo extends Component {
             })
             return
         }
-        if(!this.state.faceState&&!this.state.tempCard&&this.state.vaPerm){
+        if(!this.state.faceState&&!this.state.tempCard){
             Toast.open({
                 type:"danger",
                 content: "请记录人脸或发卡"
