5a6314243613bc4f31a32d563315c980a9abdc7f
dou <978070462@qq.com>
2020-07-20 10:45:46 +0800

    保存代码

--- stage/src/router/index.js
+++ stage/src/router/index.js
@@ -7,8 +7,11 @@ import Navgation from "../components/Navgation/index.js"
 
 // // 登录页
 import Login from "../page/Login/index"
+// // Sensid登录页
+import SensidLogin from "../page/SensidLogin/index"
 // // 首页页
 import Homepage from "../page/Homepage/index"
+import IdCard from "../components/IdCard";
 
 
 
@@ -21,12 +24,15 @@ export default class RouterIndex extends Component {
     render(){
         console.log(Common.Version)
 
+        let loginPage = Common.$_Get().idcard==3?SensidLogin:Login;
+        
+
         return (
             <Router>
                 <div>
                     <Navgation />
                     <Switch>
-                        <Route path="/login" component={ Login } />
+                        <Route path="/login" component={ loginPage } />
                         <Route path="/home" component={ Homepage } />
                         <Redirect path="/" to={{ pathname: "/login" }} />
                     </Switch>
